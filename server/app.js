07d9a67bc5ff12abb7e0a15f2dde36a17bded916
Francesco95it <francesco.spagnolo95@gmail.com>
2017-11-15 18:12:06 +0100

    added psql initial coding

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // server/app.js
 const express = require('express');
 const path = require('path');
-
+const pg = require('pg');
 const app = express();
 
 // Serve static assets
@@ -11,5 +11,16 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
+
+let pool = new pg.Pool();
+console.log(process.env.DATABASE_URL);
+pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM user', function(err, result) {
+        done();
+        if(err) return console.error("Error: "+err);
+        console.log("Result: "+result.rows);
+    });
+});
+
 
 module.exports = app;
