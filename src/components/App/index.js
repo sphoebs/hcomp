efd1ff74f52d4414a203f7b0c5c3dcdfbea8b2cb
Francesco95it <francesco.spagnolo95@gmail.com>
2017-11-26 16:43:38 +0100

    client login logic

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,19 +6,27 @@ import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props){
+        super(props);
+        if(this.props.match.params.token){
+            console.log(this.props.match.params.token);
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            </div>
+        );
+    }
 }
 
 export default App;
