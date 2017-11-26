fcc313d71283ba1325c005918ba2a6f6f9956d9e
Francesco95it <francesco.spagnolo95@gmail.com>
2017-11-26 15:41:20 +0100

    added JWT generation

--- server/utils.js
+++ server/utils.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-function generateToken(user){
-    let userInfo = {
-        name: user.name,
-        id: user.id
-    };
-    return token = jwt.sign(userInfo, process.env.JWT_SECRET, {
-        expiresIn: 60*60*24 //24h
-    });
+const util = {
+    generateToken(user) => {
+        let userInfo = {
+            name: user.name,
+            id: user.id
+        };
+        return token = jwt.sign(userInfo, process.env.JWT_SECRET, {
+            expiresIn: 60*60*24 //24h
+        });
+    },
 }
+
+module.exports = util;
