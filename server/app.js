ffb446e7ceaa94901116abec95f3ae22b563e1b6
MohamedDounnani <dounnani@gmail.com>
2018-01-16 14:41:19 +0100

    server setup and clean up

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 // libraries
 const express = require('express');
 const path = require('path');
-const Sequelize = require ('sequelize');
-const passport = require('passport');
 const session = require('express-session');
 const swaggerJSDoc = require('swagger-jsdoc');
 const util = require('util');
 const http = require('http');
 const bodyParser = require('body-parser');
+const utils = require('./utils.js');
+require('dotenv').config();
+
 
 //APP PORT AND SERVER CREATION
 const app = express();
@@ -28,63 +29,18 @@ const options = {
   
       basePath: '/'
     },
-    apis: ['api/v1.0/routes/*.js']
+    apis: ['api/routes/*.js']
   };
-  
 
-const utils = require('./utils.js');
-require('dotenv').config();
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-require('./passport_settings.js')(passport, sequelize);
 
-sequelize.authenticate().then(() => {
-    console.log('Connection to DB has been established successfully.');
-})
-.catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
-const usersTable = sequelize.import('./models/users.js');
 
-app.use(session({
+/*app.use(session({
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {maxAge: 60*60*24}
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+}));*/
 
-passport.serializeUser(function(user, cb) {
-    cb(null, {id: user.ID, name: user.EMAIL});
-});
-
-passport.deserializeUser(function(user, cb) {
-    usersTable.findById(user.id).then((user) =>{
-        cb(null, user);
-    });
-});
-
-
-app.get('/auth/login/facebook',
-    passport.authenticate('facebook', { scope: ['email'] }));
-
-app.get('/auth/login/facebook/return',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    (req, res) => {
-        //console.log(req.user);
-        res.redirect('/auth/succeded/'+utils.generateToken(req.user));
-    }
-);
-app.get('/auth/login/google',
-    passport.authenticate('google', { scope: ['email'] }));
-
-app.get('/auth/login/google/return',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req, res) => {
-        //TODO: fix redirect with react running on different server
-        res.redirect('/auth/succeded/'+utils.generateToken(req.user));
-    }
-);
 
 //SERVER CREATION
 const server = http.createServer(app);
@@ -92,6 +48,8 @@ server.listen(port, () => {
   console.log("Running on port " + port);
 });
 
+
+//SWAGGER SETUP
 const swagSpec = swaggerJSDoc(options);
 
 app.use(bodyParser.json());
@@ -104,5 +62,12 @@ app.get('/api/swagger', (req, res) => {
   res.setHeader('Content-type', 'application/json');
   res.send(swagSpec);
 });
+
+// Adding API routes
+
+require('./api/routes/Assignments')(app);
+require('./api/routes/Categories')(app);
+require('./api/routes/Tasks')(app);
+require('./api/routes/Users')(app);
 
 module.exports = app;
