7f5e1aff18ebf00a499e5418e304a408e688c7eb
MohamedDounnani <dounnani@gmail.com>
2018-01-22 15:00:46 +0100

    CORS

--- app.js
+++ app.js
@@ -51,7 +51,14 @@ app.use(session({
     cookie: {maxAge: 60*60*24}
 }));
 
-
+app.use((req, res, next) => {
+  // TODO: change this to website(react) dom when in production
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 //SERVER CREATION
 const server = http.createServer(app);
 server.listen(port, () => {
