e8c73eb76814ff039df8bd928a24f75512fea4e1
MohamedDounnani <dounnani@gmail.com>
2018-01-22 18:43:46 +0100

    min pool setup

--- knexfile.js
+++ knexfile.js
@@ -40,6 +40,13 @@ module.exports = {
       database: 'postgres://wjiugcpmwkkbsk:666402e4b8e6f0d22f617aac1c221959b7d55a610b37e694bb0a1a9b33e06671@ec2-46-137-174-67.eu-west-1.compute.amazonaws.com:5432/dfdms41acr35sl',
       user:     'wjiugcpmwkkbsk',
       password: '666402e4b8e6f0d22f617aac1c221959b7d55a610b37e694bb0a1a9b33e06671'
+    },
+    pool: {
+      min: 0,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
 
