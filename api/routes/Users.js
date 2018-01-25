0ea5a6ef1f711dedf718a8bb7557f41325fd24c7
MohamedDounnani <dounnani@gmail.com>
2018-01-25 17:15:58 +0100

    console log

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -52,6 +52,10 @@ module.exports = app => {
         personFields: 'emailAddresses,names',
         auth: oauth2Client
       }, (err, response) => {
+        console.log(response.data.id);
+        console.log(req.body.data.profileObj.googleId);
+        console.log(response.data.name);
+        console.log(req.body.data.profileObj.name);
         if (req.body.data.profileObj.googleId === response.data.id && req.body.data.profileObj.name === response.data.name) {
           return next();
         }
