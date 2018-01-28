6f48ae436950c676657766b149f82ac790639b53
MohamedDounnani <dounnani@gmail.com>
2018-01-28 11:36:25 +0100

    Test

--- api/routes/SourceOfAuth.js
+++ api/routes/SourceOfAuth.js
@@ -1,8 +1,8 @@
 var jwt = require('json-web-token');
+const users = require('../models').users;
 
 
-
-const Encode = payload => {        
+const Encode = payload => {
     const secret = 'weqsdacxzdsaewqdsacxzdsaewqdsa';
     // encode 
     const hash = jwt.encode(secret, payload, function (err, token) {
@@ -11,30 +11,44 @@ const Encode = payload => {
         } else {
             return token;
         }
-    });   
+    });
     return hash;
 }
 
-const Decode = token => {    
+const Decode = token => {
     let tmp = '';
     const secret = 'weqsdacxzdsaewqdsacxzdsaewqdsa';
     const dehash = jwt.decode(secret, token, function (err, decodedPayload, decodedHeader) {
         if (err) {
             return null;
-            
-        } else {            
-           return decodedPayload;
+
+        } else {
+            return decodedPayload;
         }
     });
     return dehash;
 }
 
-const ensureAuth1 = (req,res,next) => {
-    console.log("ciao");
-    console.log(res+req+next);
-    res.send('ok');
+const ensureAuth1 = (req, res, next) => {
+    if (req.headers.authorization) {
+        let decodedJWT = this.Decode(req.headers.authorization);
+        users
+            .findById(decodedJWT.id)
+            .then(user => {
+                if (!user) {
+                    res.status(404).send("unAuthorized Area");
+                }
+                else {
+                    return next();
+                }
+            })
+            .catch(error => res.status(400).send(error));
+    }
+    else {
+        res.status(404).send("unAuthorized Area");
+    }
 }
 
 
 
-module.exports = {Encode,Decode, ensureAuth1};
+module.exports = { Encode, Decode, ensureAuth1 };
