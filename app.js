35f3b235672f80cd21d17a70c9a0f7207eedbbdd
MohamedDounnani <dounnani@gmail.com>
2018-01-29 09:19:28 +0100

    Clean Up

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // libraries
 const express = require('express');
-const path = require('path');
 const session = require('express-session');
-const util = require('util');
 const http = require('http');
 const bodyParser = require('body-parser');
 //const Sequelize = require ('sequelize');
@@ -12,19 +10,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
-
-/*const sequelize = new Sequelize(process.env.DATABASE_URL);
-sequelize.authenticate().then(() => {
-  console.log('Connection to DB has been established successfully.');
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
-const users= sequelize.import('./api/migrations/20180117213418-users.js');
-const categories= sequelize.import('./api/migrations/20180117213418-categories.js');
-const tasks= sequelize.import('./api/migrations/20180117213418-tasks.js');
-const assignments= sequelize.import('./api/migrations/20180117213418-assignments.js');*/
-
 
 app.use(session({
     secret: 'lol',
