8f8f6e6523705eebb6f8523d5e14a35c232186a1
MohamedDounnani <dounnani@gmail.com>
2018-01-30 14:49:17 +0100

    Friends Implementation

--- api/controllers/Runs.js
+++ api/controllers/Runs.js
@@ -1,8 +1,14 @@
 const Crud = require("./Crud");
 const runs = require("../models").runs;
+const aws = require('aws-sdk');
 
-class Runs extends Crud{
-    constructor(){
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+class Runs extends Crud {
+    constructor() {
         super(runs);
         this.lastUpdated;
     }
@@ -15,9 +21,64 @@ class Runs extends Crud{
             .then(data => res.status(200).send(data.id))
             .catch(error => res.status(400).send(error));
     }
-    recentRuns(req,res){
-        
+
+    update(req, res) {        
+        return this.model
+            .findById(req.params.id)
+            .then(run => {
+                let tmp;
+                if (!run) {
+                    tmp = res.status(400).send({ message: 'Data not found!' });
+                } else {
+                    if (!req.body.image) {
+                        tmp = run
+                            .update(req.body)
+                            .then(() => res.status(200).send({ message: 'Data updated' }))
+                            .catch(error => res.status(400).send(error));
+                    }
+                    else {
+                        const s3 = new aws.S3({ params: { Bucket: process.env.S3_BUCKET } });
+                        let data = this.createData(req.body.image);
+                        s3.putObject(data, (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                console.log(result);
+                                //TODO INSERT LINK OF IMAGE
+                                tmp = run
+                                    .update({
+                                        images: {
+                                            ...data.images,
+                                            [req.body.image.number]:
+                                                req.body.image.base64
+
+                                        }
+                                    })
+                                    .then(() => res.status(200).send({ message: 'Data updated' }))
+                                    .catch(error => res.status(400).send(error));
+                            }
+                        })
+                    }
+                }
+                return tmp;
+            })
+            .catch(error => res.status(400).send(error));
     }
-} 
+
+    recentRuns(req, res) {
+
+    }
+    createData(number,image) {
+        //TODO NOME CARTELLA
+        let data = {
+            Key: number,
+            Body: image,
+            ContentEncoding: 'base64',
+            ContentType: 'image/*'
+        };
+        return data;
+    }
+}
 
 module.exports = Runs;
--- api/controllers/Users.js
+++ api/controllers/Users.js
@@ -8,6 +8,7 @@ const googleType = 'google';
 
 
 class Users extends Crud {
+  
   constructor() {
     super(users);
     this.lastUpdated;
@@ -57,28 +58,32 @@ class Users extends Crud {
               .catch(error => res.status(400).send(error));
           }
           else {
-            //CHECK IF ACCESS TOKEN IS DIFFERENT, IF SO CHANGE IT
-            if (user.accessToken !== data.accessToken) {
-              user.update({
-                accessToken: data.accessToken
-              })
-                .then(user => {
-                  let payload = {
-                    id: user.id
-                  }
-                  let hash = Encode(payload);
-                  let sendResponse = this.createPayload(user.id, hash);
-                  tmp = res.status(200).send(JSON.stringify(sendResponse));
+            if (isWriter === user.creator) {
+              if (user.accessToken !== data.accessToken) {
+                user.update({
+                  accessToken: data.accessToken
                 })
-                .catch(error => res.status(400).send(error));
+                  .then(user => {
+                    let payload = {
+                      id: user.id
+                    }
+                    let hash = Encode(payload);
+                    let sendResponse = this.createPayload(user.id, hash);
+                    tmp = res.status(200).send(JSON.stringify(sendResponse));
+                  })
+                  .catch(error => res.status(400).send(error));
+              }
+              else {
+                let payload = {
+                  id: user.id
+                }
+                let hash = Encode(payload);
+                let sendResponse = this.createPayload(user.id, hash);
+                tmp = res.status(200).send(JSON.stringify(sendResponse));
+              }
             }
             else {
-              let payload = {
-                id: user.id
-              }
-              let hash = Encode(payload);
-              let sendResponse = this.createPayload(user.id, hash);
-              tmp = res.status(200).send(JSON.stringify(sendResponse));
+              tmp = res.status(400).send({ message: 'Something goes Wrong!' })
             }
           }
           return tmp;
@@ -116,35 +121,39 @@ class Users extends Crud {
                   }
                   let hash = Encode(payload);
                   let sendResponse = this.createPayload(user.id, hash);
-                  console.log(sendResponse);
                   tmp = res.status(200).send(JSON.stringify(sendResponse));
                 }
                 return tmp;
               })
               .catch(error => res.status(400).send(error));
           } else {
-            //CHECK IF ACCESS TOKEN IS DIFFERENT, IF SO CHANGE IT
-            if (user.accessToken !== data.accessToken) {
-              user.update({
-                accessToken: data.accessToken
-              })
-                .then(user => {
-                  let payload = {
-                    id: user.id
-                  }
-                  let hash = Encode(payload);
-                  let sendResponse = this.createPayload(user.id, hash);
-                  tmp = res.status(200).send(JSON.stringify(sendResponse));
+            if (isWriter === user.creator) {
+              //CHECK IF ACCESS TOKEN IS DIFFERENT, IF SO CHANGE IT
+              if (user.accessToken !== data.accessToken) {
+                user.update({
+                  accessToken: data.accessToken
                 })
-                .catch(error => res.status(400).send(error));
+                  .then(user => {
+                    let payload = {
+                      id: user.id
+                    }
+                    let hash = Encode(payload);
+                    let sendResponse = this.createPayload(user.id, hash);
+                    tmp = res.status(200).send(JSON.stringify(sendResponse));
+                  })
+                  .catch(error => res.status(400).send(error));
+              }
+              else {
+                let payload = {
+                  id: user.id
+                }
+                let hash = Encode(payload);
+                let sendResponse = this.createPayload(user.id, hash);
+                tmp = res.status(200).send(JSON.stringify(sendResponse));
+              }
             }
             else {
-              let payload = {
-                id: user.id
-              }
-              let hash = Encode(payload);
-              let sendResponse = this.createPayload(user.id, hash);
-              tmp = res.status(200).send(JSON.stringify(sendResponse));
+              tmp = res.status(400).send({ message: 'Something Goes Wrong!' })
             }
           }
           return tmp;
@@ -187,3 +196,34 @@ class Users extends Crud {
 
 
 module.exports = Users;
+
+
+//FACEBOOK API CALL FOR TAKING FRIEND THAT USE OUT APP
+
+/*
+  FB.api('/me/friends', (response) => {
+      if(response.data) {
+         console.log(response.data)
+      } else {
+          alert("Error!");
+      }
+  });
+  */
+
+//POSSIBLE IMPLEMENTATION FOR HAVING USER FRIENDS ON GOOGLE
+
+/*oauth2Client.setCredentials({
+  access_token: req.body.data.accessToken
+});
+plus.people.get({
+  userId: 'me/friends',
+  personFields: 'emailAddresses,names',
+  auth: oauth2Client
+}, (err, response) => {
+  if (req.body.data.profileObj.googleId === response.data.id && req.body.data.profileObj.name === response.data.displayName) {
+    return next();
+  }
+  else {
+    res.status(404).send("unAuthorized Area");
+  }
+});*/
