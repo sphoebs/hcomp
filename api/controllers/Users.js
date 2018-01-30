a3e19fcca3a8225c78434c89b742e4565ce4b246
MohamedDounnani <dounnani@gmail.com>
2018-01-30 11:07:53 +0100

    Update User Login

--- api/controllers/Users.js
+++ api/controllers/Users.js
@@ -1,7 +1,7 @@
 const Crud = require("./Crud");
 const users = require("../models").users;
 const app = require("../../app").app;
-const {Encode} = require('../Security/SourceOfAuth');
+const { Encode } = require('../Security/SourceOfAuth');
 
 const facebookType = 'facebook';
 const googleType = 'google';
@@ -17,7 +17,7 @@ class Users extends Crud {
   //controllare con access token
   create(req, res) {
     let data = req.body.data;
-    let type = req.body.type;    
+    let type = req.body.type;
     let isWriter = req.body.isWriter;
     if (type === facebookType) {
       return this.model
@@ -47,21 +47,21 @@ class Users extends Crud {
                 else {
                   let payload = {
                     id: user.id
-                  }                  
+                  }
                   let hash = Encode(payload);
-                  let sendResponse = this.createPayload(user.id,hash);                
+                  let sendResponse = this.createPayload(user.id, hash);
                   tmp = res.status(200).send(JSON.stringify(sendResponse));
                 }
                 return tmp;
               })
               .catch(error => res.status(400).send(error));
           }
-          else {     
+          else {
             let payload = {
               id: user.id
-            }            
+            }
             let hash = Encode(payload);
-            let sendResponse = this.createPayload(user.id,hash);           
+            let sendResponse = this.createPayload(user.id, hash);
             tmp = res.status(200).send(JSON.stringify(sendResponse));
           }
           return tmp;
@@ -96,9 +96,9 @@ class Users extends Crud {
                 else {
                   let payload = {
                     id: user.id
-                  }                  
+                  }
                   let hash = Encode(payload);
-                  let sendResponse = this.createPayload(user.id,hash);
+                  let sendResponse = this.createPayload(user.id, hash);
                   console.log(sendResponse);
                   tmp = res.status(200).send(JSON.stringify(sendResponse));
                 }
@@ -108,9 +108,9 @@ class Users extends Crud {
           } else {
             let payload = {
               id: user.id
-            }           
+            }
             let hash = Encode(payload);
-            let sendResponse = this.createPayload(user.id,hash);
+            let sendResponse = this.createPayload(user.id, hash);
             console.log(sendResponse);
             tmp = res.status(200).send(JSON.stringify(sendResponse));
           }
@@ -121,16 +121,89 @@ class Users extends Crud {
   }
 
 
+  findOrCreate(req, res) {
+    let data = req.body.data;
+    let type = req.body.type;
+    let isWriter = req.body.isWriter;
+    if (type === facebookType) {
+      return this.model
+        .findOrCreate({
+          where: {
+            social_id: data.id,
+            email: data.email,
+            name: data.name,
+            img: data.picture.data.url,
+            creator: isWriter,
+            access_type: facebookType
+          }
+        })
+        .then(user => {
+          let tmp;
+          if (!user) {
+            tmp = res, status(404).send({ message: 'Something goes wrong' });
+          }
+          else {
+            user.update({
+              accessToken: data.accessToken
+            })
+              .then(user => {
+                let payload = {
+                  id: user.id
+                }
+                let hash = Encode(payload);
+                let sendResponse = this.createPayload(user.id, hash);
+                tmp = res.status(200).send(JSON.stringify(sendResponse));
+              }).catch(error => res.status(400).send(error));
+          }
+          return tmp;
+        })
+        .catch(error => res.status(400).send(error));
+    }
+    else {
+      return this.model
+        .findOrCreate({
+          where: {
+            social_id: data.profileObj.googleId,
+            email: data.profileObj.email,
+            img: data.profileObj.imageUrl,
+            name: data.profileObj.name,
+            creator: isWriter,
+            access_type: googleType
+          }
+        })
+        .then(user => {
+          let tmp;
+          if (!user) {
+            tmp = res, status(404).send({ message: 'Something goes wrong' });
+          } else {
+            user.update({
+              accessToken: data.accessToken
+            })
+              .then(user => {
+                let payload = {
+                  id: user.id
+                }
+                let hash = Encode(payload);
+                let sendResponse = this.createPayload(user.id, hash);
+                tmp = res.status(200).send(JSON.stringify(sendResponse));
+              }).catch(error => res.status(400).send(error));
+          }
+          return tmp;
+        })
+        .catch(error => res.status(400).send(error));
+    }
+  }
+
   readAll(req, res) {
     return this.model
-      .findAll({ attributes: ['name','img','email','createdAt']})
+      .findAll({ attributes: ['name', 'img', 'email', 'createdAt'] })
       .then(users => res.status(200).send(users))
       .catch(error => res.status(400).send(error));
   }
 
-  readOne(req, res) {    
+  readOne(req, res) {
     return this.model
-      .findById(req.params.id, { attributes: ['name','img','email','createdAt']})
+      .findById(req.params.id, { attributes: ['name', 'img', 'email', 'createdAt'] })
       .then(user => {
         let tmp;
         if (!user) {
@@ -143,7 +216,7 @@ class Users extends Crud {
       .catch(error => res.status(400).send(error));
   }
 
-  createPayload(id,jwt){
+  createPayload(id, jwt) {
     let payload = {
       id: id,
       jwt: jwt
