06837fbd2877731eb20c535caeb250f7bb1f83b4
MohamedDounnani <dounnani@gmail.com>
2018-02-02 14:56:58 +0100

    Test

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ server.listen(port, () => {
 });
 
 app.use(bodyParser({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true , limit: '50mb'})) // handle URL-encoded data
+//app.use(bodyParser.urlencoded({ extended: true , limit: '50mb'})) // handle URL-encoded data
 
 // Adding API routes
 
