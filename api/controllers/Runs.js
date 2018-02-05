42ba9b55cccec435bc7b4d5a2091ab9c8c0eb447
MohamedDounnani <dounnani@gmail.com>
2018-02-05 21:27:45 +0100

    Test

--- api/controllers/Runs.js
+++ api/controllers/Runs.js
@@ -164,6 +164,7 @@ class Runs extends Crud {
                             .catch(error => res.status(400).send(error));
                     }
                     if (req.body.imgname) {
+                        console.log(req.body.imgname);
                         console.log("SINGLE IMAGE");
                         let imageKey = tasksName + data.id_task + '/' + runsName + data.id + '/' + req.body.imgname;
                         s3.deleteObject({ Key: imageKey }, (err, response) => {
