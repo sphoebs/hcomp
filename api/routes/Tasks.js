80c364fb31b66ed9d6c40d9cf5a3fa2abc147b31
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-16 12:45:00 +0100

    Test

--- api/routes/Tasks.js
+++ api/routes/Tasks.js
@@ -2,13 +2,13 @@ const controller = require('../controllers').tasks;
 const {ensureAuthorization, ensureAuthorizationCreator} = require('../Utility/Utility');
 module.exports = app => {
    
-    app.post("/tasks", ensureAuthorizationCreator,(req,res) => {       
+    app.post("/tasks", ensureAuthorizationCreator,(req,res) => {
         controller.create(req,res);
     });
-    app.get("/tasks", ensureAuthorization,(req,res) => {
+    app.get("/tasks",(req,res) => {
         controller.readAll(req,res);
     })
-    app.get("/tasks/:id" , ensureAuthorization, (req,res) => {
+    app.get("/tasks/:id" , (req,res) => {
         controller.readOne(req,res);
     });
     app.put("/tasks/:id",ensureAuthorizationCreator, (req,res) => {
@@ -16,11 +16,11 @@ module.exports = app => {
     });
     app.delete("/tasks/:id",ensureAuthorizationCreator, (req,res)=> {
         controller.delete(req,res);
-    });   
+    });
     app.get("/tasks/creatorRecentTasks:id", ensureAuthorizationCreator, (req,res) => {
         controller.creatorRecentTasks(req,res);
-    });  
-   
+    });
+
     app.patch('/tasks', ensureAuthorizationCreator, (req,res) => {
         controller.deletePhoto(req,res);
     })
