c5d2d7cba19c665104842f8a62025c56bbe38a08
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-19 21:17:41 +0100

    Test

--- api/Utility/Utility.js
+++ api/Utility/Utility.js
@@ -1,136 +1,108 @@
-var jwt = require('json-web-token');
-const users = require('../models').users;
+var jwt = require("json-web-token");
+const users = require("../models").users;
 
-const facebookType = 'facebook';
+const facebookType = "facebook";
 
-const googleType = 'google';
+const googleType = "google";
 
-const url_images = 'https://s3.eu-central-1.amazonaws.com/socialhumancomputationproject/';
-const tasksName = 'tasks';
-const runsName = 'runs';
+const url_images =
+  "https://s3.eu-central-1.amazonaws.com/socialhumancomputationproject/";
+const tasksName = "tasks";
+const runsName = "runs";
 const Encode = payload => {
-    const secret = process.env.secretHSC;
-    // encode 
-    const hash = jwt.encode(secret, payload, (err, token) => {
-        if (err) {
-            console.error(err.name, err.message);
-        } else {
-            return token;
-        }
-    });
-    return hash;
-}
+  const secret = process.env.secretHSC;
+  // encode
+  const hash = jwt.encode(secret, payload, (err, token) => {
+    if (err) {
+      console.error(err.name, err.message);
+    } else {
+      return token;
+    }
+  });
+  return hash;
+};
 
 const Decode = token => {
-    const secret = process.env.secretHSC;
-    const dehash = jwt.decode(secret, token, (err, decodedPayload, decodedHeader) => {
-        if (err) {
-            return null;
-
-        } else {
-            return decodedPayload;
-        }
-    });
-    return dehash;
-}
+  const secret = process.env.secretHSC;
+  const dehash = jwt.decode(
+    secret,
+    token,
+    (err, decodedPayload, decodedHeader) => {
+      if (err) {
+        return null;
+      } else {
+        return decodedPayload;
+      }
+    }
+  );
+  return dehash;
+};
 
 /*DECODE THE JWT INSIDE HEADERS AUTHORIZATION AND CHECK IF ID IS IN DATABASE
  IF GO NEXT ELSE ERROR
  */
 
 const ensureAuthorization = (req, res, next) => {
-    if (req.headers.authorization) {
-        let decodedJWT = Decode(req.headers.authorization);
-        console.log(decodedJWT);
-        users
-            .findById(decodedJWT.id)
-            .then(user => {
-                if (!user) {
-                    res.status(404).send("unAuthorized Area");
-                }
-                else {
-                    return next();
-                }
-            })
-            .catch(error => res.status(400).send(error));
-    }
-    else {
-        res.status(404).send("unAuthorized Area");
-    }
-}
+  if (req.headers.authorization) {
+    let decodedJWT = Decode(req.headers.authorization);
+    console.log(decodedJWT);
+    users
+      .findById(decodedJWT.id)
+      .then(user => {
+        if (!user) {
+          res.status(404).send("unAuthorized Area");
+        } else {
+          return next();
+        }
+      })
+      .catch(error => res.status(400).send(error));
+  } else {
+    res.status(404).send("unAuthorized Area");
+  }
+};
 /*DECODE THE JWT INSIDE HEADERS AUTHORIZATION AND CHECK IF ID IS IN DATABASE AND IF IS CREATOR
   IF GO NEXT ELSE ERROR
   */
 const ensureAuthorizationCreator = (req, res, next) => {
-    if (req.headers.authorization) {
-        let decodedJWT = Decode(req.headers.authorization);
-        users
-            .findOne({ where: { id: decodedJWT.id, creator: true } })
-            .then(user => {
-                if (!user) {
-                    console.log("non trova utente");
-                    res.status(404).send("unAuthorized Area");
-                }
-                else {
-                    return next();
-                }
-            })
-            .catch(error => res.status(400).send(error));
-    }
-    else {
-        console.log("Non trova il jwt")
-        res.status(404).send("unAuthorized Area");
-    }
-}
-
-const readQuery = (elementSearched, url) => {
-    elementSearched = elementSearched.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + elementSearched + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-//CHECK IF THERE IS ONLY ONE TRUE
-const securityControl = (id_task, id_runtype, id_run) => {
-    let arrayOfBooleans = [id_task, id_run, id_runtype];
-    if (id_task && id_runtype && id_run) {
-        for (let i = 0; i < arrayOfBooleans.length; i++) {
-            if (!arrayOfBooleans[i]) {
-                return true;
-            }
+  let decodedJWT = Decode(req.headers.authorization);
+  if (decodedJWT) {
+    users
+      .findOne({ where: { id: decodedJWT.id, creator: true } })
+      .then(user => {
+        if (!user) {
+          console.log("non trova utente");
+          res.status(404).send("unAuthorized Area");
+        } else {
+          return next();
         }
-        return false;
-    }
-    else {
-        return false;
-    }
-}
+      })
+      .catch(error => res.status(400).send(error));
+  } else {
+    console.log("Non trova il jwt");
+    res.status(404).send("unAuthorized Area");
+  }
+};
 
-const createData = (image, name) => {    
-    let data = {
-        Key: name,
-        Body: image,
-        ContentEncoding: 'base64',
-        ContentType: 'image/*',
-        ACL: 'public-read'
-    };
-    return data;
-}
-
+const createData = (image, name) => {
+  let data = {
+    Key: name,
+    Body: image,
+    ContentEncoding: "base64",
+    ContentType: "image/*",
+    ACL: "public-read"
+  };
+  return data;
+};
 
 module.exports = {
-    Encode,
-    Decode,
-    ensureAuthorization,
-    ensureAuthorizationCreator,
-    readQuery,
-    securityControl,
-    facebookType,
-    googleType,
-    url_images,
-    createData,
-    runsName,
-    tasksName
+  Encode,
+  Decode,
+  ensureAuthorization,
+  ensureAuthorizationCreator,
+  facebookType,
+  googleType,
+  url_images,
+  createData,
+  runsName,
+  tasksName
 };
