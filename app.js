03bd2e881b869f9c129775d900eeaaf6d8e940d8
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-19 21:03:44 +0100

    Test

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
  res.setHeader('Access-Control-Allow-Origin', '*');
   /*res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();*/
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");*/
+  next();
 });
 //SERVER CREATION
 const server = http.createServer(app);
