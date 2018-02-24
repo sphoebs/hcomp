91d31a5297eaf75b10701448accb20439865c28a
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-24 21:04:12 +0100

    Test

--- api/controllers/Tasks.js
+++ api/controllers/Tasks.js
@@ -265,7 +265,7 @@ class Tasks extends Crud {
         console.log(err);
       } else {
         const id = parseInt(req.params.id);
-        const query = `SELECT u.name FROM users AS u INNER JOIN tasks AS t WHERE  u.id = t.id_creator AND u.id = ${id};`;
+        const query = `SELECT u.name FROM users AS u INNER JOIN tasks AS t WHERE u.id = t.id_creator AND u.id = ${id};`;
         client.query(query, (err, result) => {
           done();
           if (err) {
