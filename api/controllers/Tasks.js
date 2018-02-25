c516f07a3c7d888ff14f2bb82b5ce98233edbdd8
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-25 20:13:12 +0100

    Test

--- api/controllers/Tasks.js
+++ api/controllers/Tasks.js
@@ -122,7 +122,8 @@ class Tasks extends Crud {
             return  res.status(500).send({ message: "INTERNAL ERROR" });
             console.log(err);
           } else {
-            const query = `SELECT t.* FROM tasks AS t INNER JOIN assignments AS a ON t.id=a.id_task ORDER BY a.id DESC LIMIT 4;`;
+            const id = 'a.updatedAt';
+            const query = `SELECT t.* FROM tasks AS t INNER JOIN assignments AS a ON t.id=a.id_task ORDER BY ${id} DESC LIMIT 4;`;
             client.query(query, (err, result) => {
               done();
               if (err) {
