7db44b05af4fabe23747f38cce69b3b30f319c73
Mohamed Dounnani <mohamed.dounnani@studenti.unitn.it>
2018-02-27 21:14:55 +0100

    Test

--- api/controllers/Assignments.js
+++ api/controllers/Assignments.js
@@ -238,6 +238,7 @@ class Assignments extends Crud {
             let oldStatistics = run.statistics ? run.statistics : {};
             console.log(oldStatistics);
             answers.forEach(answer => {
+              console.log(Object.keys(oldStatistics).length);
               let oldAnswers = Object.keys(oldStatistics).length>0 ? JSON.parse(oldStatistics[answer.imgname]) : {};
               let yesPercentOfAnswers = oldAnswers.yes ? oldAnswers.yes : 0;
               let noPercentOfAnswers = oldAnswers.no ? oldAnswers.no : 0;
